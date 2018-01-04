6ad7892b737fe2a7dbe3ef58c15c5f4b44a01606
Kaunil Dhruv <dhruv.kaunil@gmail.com>
2018-01-04 19:23:47 +0530

    Registering KeyPresses

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,58 +1,73 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Key from '../Key';
 
 import './Keyboard.css'
 var utils = require('../../utils');
 
 class Keyboard extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      type: props.type,
-      language: props.language
+    constructor(props){
+        super(props);
+        this.state = {
+            type: props.type,
+            language: props.language
+        }
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
-  }
-  render(){
-    const rows = utils[this.state.type][this.state.language];
 
-    return(
-      <div className="Keyboard">
-        <div className='first'>
-        {
-          rows.first.map((key) =>{
-            return <Key key={key} value={key} />
-          })
-        }
-        </div>
-        <div className='second'>
-        {
-          rows.second.map((key) =>{
-            return <Key key={key} value={key} />
-          })
-        }
-        </div>
-        <div className='third'>
-        {
-          rows.third.map((key) =>{
-            return <Key key={key} value={key} />
-          })
-        }
-        </div>
-        <div className='fourth'>
-        {
-          rows.fourth.map((key) =>{
-            return <Key key={key} value={key} />
-          })
-        }
-        </div>
-        <div className="fourth">
-          <Key value="space">
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyDown)
+    }
 
-          </Key>
-        </div>
-      </div>
-    )
-  }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown)
+    }
+
+    handleKeyDown(event){
+        ReactDOM.findDOMNode(this.refs[event.key]).classList.toggle('pressed');
+    }
+
+    render(){
+        const rows = utils[this.state.type][this.state.language];
+
+        return(
+            <div className="Keyboard">
+                <div className='first'>
+                    {
+                        rows.first.map((key) =>{
+                            return <Key ref={key} key={key} value={key} />
+                        })
+                    }
+                </div>
+                <div className='second'>
+                    {
+                        rows.second.map((key) =>{
+                            return <Key key={key} ref={key} value={key} />
+                        })
+                    }
+                </div>
+                <div className='third'>
+                    {
+                        rows.third.map((key) =>{
+                            return <Key key={key} ref={key} value={key} />
+                        })
+                    }
+                </div>
+                <div className='fourth'>
+                    {
+                        rows.fourth.map((key) =>{
+                            return <Key key={key} ref={key} value={key} />
+                        })
+                    }
+                </div>
+                <div className="fourth">
+                    <Key value="space">
+
+                    </Key>
+                </div>
+            </div>
+        )
+    }
 
 }
 
